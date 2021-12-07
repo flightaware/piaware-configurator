47ac39b78125122070ae3b346dd4b6d10370af20
eric1tran <eric1tran@gmail.com>
2021-12-07 21:05:53 +0000

    Bind piaware-configurator to localhost

--- etc/piaware-configurator/piaware-configurator.conf.py
+++ etc/piaware-configurator/piaware-configurator.conf.py
@@ -1,7 +1,7 @@
 # Gunicorn configuration file
 
 # bind - what ports/sockets to listen on
-bind = '0.0.0.0:5000'
+bind = '127.0.0.1:5000'
 
 # loglevel - "debug", "info", "warning", "error", "critical"
 loglevel = 'info'
