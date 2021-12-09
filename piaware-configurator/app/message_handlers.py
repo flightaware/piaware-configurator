787fdbb1698fde81736b388ced5bee4913f00077
eric1tran <eric1tran@gmail.com>
2021-12-09 17:53:52 +0000

    Run piaware-configurator as its own user with a few sudo rules to allow configuring piaware settings and restarting the network

--- piaware-configurator/app/message_handlers.py
+++ piaware-configurator/app/message_handlers.py
@@ -198,8 +198,7 @@ def handle_set_wifi_config_request(config_request):
             set_piaware_config(config, value)
 
         current_app.logger.info(f'Restarting network...')
-        cmd = ["sudo", "piaware-restart-network"]
-        _ = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        tohil.eval('::fa_sudo::exec_as -root -- /usr/bin/piaware-restart-network')
 
         json_response, status_code = {"success": True}, HTTPStatus.OK
     except KeyError:
