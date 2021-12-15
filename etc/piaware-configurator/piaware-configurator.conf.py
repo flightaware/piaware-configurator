0fa05693eeed47f3b7d305703a834eedd1033236
eric1tran <eric1tran@gmail.com>
2021-12-15 17:13:24 +0000

    Use 2 workers and increase timeout

--- etc/piaware-configurator/piaware-configurator.conf.py
+++ etc/piaware-configurator/piaware-configurator.conf.py
@@ -5,3 +5,9 @@ bind = '127.0.0.1:5000'
 
 # loglevel - "debug", "info", "warning", "error", "critical"
 loglevel = 'info'
+
+# Number of worker processes
+workers = 2
+
+# Worker timeout (seconds)
+timeout = 120
